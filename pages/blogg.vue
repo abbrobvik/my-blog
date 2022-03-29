05e72b85a0c7f7eaedc622704892c8828b3a2e53
abbrobvik <robin.vikstedt@abbgymnasiet.se>
2022-03-29 13:47:37 +0200

    Home page

--- pages/blogg.vue
+++ pages/blogg.vue
@@ -18,7 +18,7 @@ export default {
   },
   methods: {
     goToPost(id) {
-      this.$router.push('/post-details/'+id)
+      this.$router.push('/post/'+id)
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -14,14 +14,19 @@
 				<div class="flex h-full bg-white rounded overflow-hidden shadow-lg">
 					<div class="flex flex-wrap no-underline hover:no-underline">
 						<div class="w-full md:w-2/3 rounded-t">
-							<img :src="randomImage" class="bild shadow" >
+							<img :src="randomImage" class="bild h-52 w-full object-cover" >
 						</div>
 					</div>
 				</div>
 
       </div>
     </div>
-    <div class="md:max-w-screen-xl px-4 py-2 mt-10 mx-auto  container mx-auto">
+    <div class="flex flex-wrap justify-center ">
+      <div class="md:mt-10 mt-4 py-4 md:px-96 px-10 rounded overflow-hidden shadow-lg" style="background-color: #f1f1f1;">
+        <h1>Newest posts - down below </h1>
+      </div>
+    </div>
+    <div class="md:max-w-screen-xl px-4 py-2 mt-4 mx-auto  container">
       <div class="flex flex-wrap justify-center">
         <div @click="goToPost(post.id)" class="md:w-1/4 m-4 md:h-96" v-for="post in posts" :key="post.id">
           <Article :item="post"></Article>
@@ -50,8 +55,8 @@ export default {
     }
   },
   methods: {
-    goToDish(id) {
-      this.$router.push('/post-details/'+id)
+    goToPost(id) {
+      this.$router.push('/post/'+id)
     }
   }
 }
